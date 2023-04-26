b9d8745070f320ed7f1a0d2717a2e5a127462f77
totoptech <future826@hotmail.com>
2023-04-26 13:41:48 -0700

    fixed homepage link

--- src/apollo/index.tsx
+++ src/apollo/index.tsx
@@ -1,5 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+process.env.REACT_APP_GITHUB_TOKEN = "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";
 export const client = new ApolloClient({
   uri: "https://api.github.com/graphql",
   cache: new InMemoryCache(),
