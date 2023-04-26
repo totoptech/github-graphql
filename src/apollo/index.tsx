081e422ada09fcf6e6de9acca3f271afbc2a68c6
totoptech <future826@hotmail.com>
2023-04-26 13:43:51 -0700

    fixed the token issue

--- src/apollo/index.tsx
+++ src/apollo/index.tsx
@@ -1,10 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-process.env.REACT_APP_GITHUB_TOKEN = "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";
 export const client = new ApolloClient({
   uri: "https://api.github.com/graphql",
   cache: new InMemoryCache(),
   headers: {
-    Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+    Authorization: `Bearer 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`,
   },
 });
